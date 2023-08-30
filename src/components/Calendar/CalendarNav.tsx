5a701ac352ce9b8acb6bd0ee34e984f16dec0d53
DutsAndrew <dutsandrew@gmail.com>
2023-08-29 18:02:07 -0600

    calendar app nav added, fully styled

--- src/components/Calendar/CalendarNav.tsx
+++ src/components/Calendar/CalendarNav.tsx
@@ -1,12 +1,65 @@
 import React from "react";
+import styles from '../../styles/components/Calendar/calendar.module.css';
+import plusSVG from '../../assets/plus.svg';
+import leftArrowSvg from '../../assets/chevron-left.svg';
+import rightArrowSvg from '../../assets/chevron-right.svg';
+import menuDownSvg from '../../assets/menu-down.svg';
 
 const CalendarNav = () => {
   return (
-    <main>
-      <p>
-        CalendarNav
-      </p>
-    </main>
+    <nav className={styles.calendarNavContainer}>
+      <div className={styles.calendarNavContainerLeft}>
+        <img
+          className={styles.plusSvg}
+          alt="plus icon"
+          src={plusSVG}>
+        </img>
+        <div className={styles.navigationArrowsContainer}>
+          <img
+            className={styles.navigationArrowSvg}
+            alt="left arrow"
+            src={leftArrowSvg}>
+          </img>
+          <img
+            className={styles.navigationArrowSvg}
+            alt="right arrow"
+            src={rightArrowSvg}>
+          </img>
+        </div>
+      </div>
+      <div className={styles.calendarNavContainerRight}>
+        <div className={styles.yearDropDownContainer}>
+          <p className={styles.yearDropDownText}>
+            Year
+          </p>
+          <img
+            className={styles.yearDropDownSvg}
+            alt="down arrow"
+            src={menuDownSvg}>
+          </img>
+        </div>
+        <div className={styles.calendarDropDownContainer}>
+          <p className={styles.calendarDropDownText}>
+            (0) Calendars
+          </p>
+          <img
+            className={styles.calendarDropDownSvg}
+            alt="down arrow"
+            src={menuDownSvg}>
+          </img>
+        </div>
+        <div className={styles.viewDropDownContainer}>
+          <p className={styles.viewDropDownText}>
+            All
+          </p>
+          <img
+            className={styles.viewDropDownSvg}
+            alt="down arrow"
+            src={menuDownSvg}>
+          </img>
+        </div>
+      </div>
+    </nav>
   );
 };
 
