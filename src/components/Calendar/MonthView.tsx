214d70a4481e80ef50a2b3b176674363386fc6f2
DutsAndrew <dutsandrew@gmail.com>
2023-09-05 17:39:38 -0600

    index for getting three month preview set

--- src/components/Calendar/MonthView.tsx
+++ src/components/Calendar/MonthView.tsx
@@ -16,9 +16,36 @@ const MonthView:FC<monthViewProps> = (props): JSX.Element => {
   // 7. Search through calendar holiday's and check if any matches for the current calendar cycle, render them if needed
   // 8. Search through events array, find matches, render them to current calendar cycle
 
+  const calendarMonths = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December",
+  ];
+
+  const getTodaysYear = () => {
+    const year = new Date().getFullYear();
+    return year;
+  };
+
+  const getTodaysMonth = () => {
+    const month = new Date().getMonth();
+    return month;
+  };
+
+  const getThreeMonthPreview = () => {
+    const todaysYear = getTodaysYear();
+    const currentYearAndDates = personalCalendar.calendar_years_and_dates[`${todaysYear}`];
+
+    const month = getTodaysMonth();
+    const currentMonth = calendarMonths[month];
+
+    console.log(todaysYear, currentYearAndDates, currentMonth);
+  };
+
+  getThreeMonthPreview();
+
   return (
     <section>
-      
+
     </section>
   );
 };
