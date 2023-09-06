5c519719d7155bf78225ee4a7eeda76bd60a8bfc
DutsAndrew <dutsandrew@gmail.com>
2023-09-05 19:57:47 -0600

    calendar days render to page for the entire month appropriately, styles differentiate valid and invalid days to maintain calendar grid

--- src/components/Calendar/MonthView.tsx
+++ src/components/Calendar/MonthView.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { monthViewProps } from "../../types/interfaces";
+import styles from '../../styles/components/Calendar/calendar.module.css';
 
 const MonthView:FC<monthViewProps> = (props): JSX.Element => {
 
@@ -16,6 +17,10 @@ const MonthView:FC<monthViewProps> = (props): JSX.Element => {
   // 7. Search through calendar holiday's and check if any matches for the current calendar cycle, render them if needed
   // 8. Search through events array, find matches, render them to current calendar cycle
 
+  const week = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+  ];
+
   const calendarMonths = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December",
@@ -31,21 +36,65 @@ const MonthView:FC<monthViewProps> = (props): JSX.Element => {
     return month;
   };
 
-  const getThreeMonthPreview = () => {
+  const getMonthPreview = () => {
     const todaysYear = getTodaysYear();
-    const currentYearAndDates = personalCalendar.calendar_years_and_dates[`${todaysYear}`];
+    const currentYearAndDates = personalCalendar.calendar_years_and_dates[todaysYear];
 
     const month = getTodaysMonth();
     const currentMonth = calendarMonths[month];
 
-    console.log(todaysYear, currentYearAndDates, currentMonth);
+    return currentYearAndDates[currentMonth];
   };
 
-  getThreeMonthPreview();
+  const generateMonthCalendar = () => {
+    const preview: any = getMonthPreview();
+    const monthStartsOn = preview.month_starts_on;
+    const numberOfDays = preview.days;
+
+    const calendar = [];
+
+    const weekStartIndex = week.indexOf(monthStartsOn);
+    const numberOfPrecedingDays = week.slice(0, weekStartIndex).length;
+    
+    for (let i = 0; i < numberOfPrecedingDays; i++) {
+      calendar.push(`${week[i]}`);
+    };
+
+    let weekLoopIndex = weekStartIndex;
+    for (let i = 1; i < numberOfDays + 1; i++) {
+      calendar.push(`${i}-${week[weekLoopIndex]}`)
+      if (weekLoopIndex === week.length - 1) {
+        weekLoopIndex = 0;
+      } else {
+        weekLoopIndex++;
+      };
+    };
+
+    return calendar;
+  };
 
   return (
-    <section>
+    <section className={styles.monthViewContainer}>
+      <div className={styles.monthItemsContainer}>
+        {generateMonthCalendar().map((item) => {
+          const isAccurateMonthDate = item.includes('-');
+          const containerClass = isAccurateMonthDate
+          ? styles.monthItemValidDateContainer
+          : styles.monthItemInvalidDateContainer;
 
+          return <div 
+            key={item}
+            className={`${styles.monthItemContainer} ${containerClass}`}
+          >
+            <p className={styles.monthItemDateNumberText}>
+              {item.includes('-') ? item.split('-')[0] : ''}
+            </p>
+            <p className={styles.monthItemWeekDayText}>
+              {item.includes('-') ? item.split('-')[1] : item}
+            </p>
+          </div>
+        })}
+      </div>
     </section>
   );
 };
