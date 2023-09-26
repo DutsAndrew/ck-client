5241daaedfe440fdd5bb5ae2f0fdd923f23f0a1f
DutsAndrew <dutsandrew@gmail.com>
2023-09-25 18:14:13 -0600

    edit calendar edits from previous commit

--- src/components/Calendar/EditCalendar.tsx
+++ src/components/Calendar/EditCalendar.tsx
@@ -12,9 +12,24 @@ const EditCalendar:FC<EditCalendarProps> = (props): JSX.Element => {
 
   console.log(selectedCalendar);
 
+  // Edit Calendar should have the following:
+    // 3 tables of user lists for: authorized, view-only, pending-invites/requests
+      // each table should have the users in it with their first and last name
+      // each user listed should have an edit or delete button, but greyed out unless the user
+      // is in the authorized list
+
+    // addition button next to user list tables to add users to any list
+
+    // delete calendar button
+      // users cannot delete their own personal calendar
+
+    // all events in the calendar
+      // each event should have an edit and delete button, but greyed out if user isn't authed
+
+
   return (
     <section className={styles.editCalendarContainer}>
-      <button onClick={() => handleCloseCalendarEditor()}>close</button>
+      <button onClick={() => handleCloseCalendarEditor()}>Close Editor</button>
       <p>Edit</p>
     </section>
   );
