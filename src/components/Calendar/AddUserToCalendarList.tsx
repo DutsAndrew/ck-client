8d6ba9e7aa7e8b3c7e1f2327f3c8ad368d754fc9
DutsAndrew <dutsandrew@gmail.com>
2023-10-31 18:20:16 -0600

    addUserToCalendar component built out with api call linked in

--- src/components/Calendar/AddUserToCalendarList.tsx
+++ src/components/Calendar/AddUserToCalendarList.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styles from '../../styles/components/Calendar/calendar.module.css';
 import { AddUserToCalendarListProps } from "../../types/interfaces";
+import searchSvg from '../../assets/magnify.svg';
+import { json } from "stream/consumers";
 
 const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Element => {
 
@@ -8,6 +10,48 @@ const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Elemen
     handleAddUserClick, 
     addUserActivated 
   } = props;
+
+  const [userLookup, setUserLookup] = useState('');
+  const [userQueryResults, setUserQueryResults] = useState([]);
+
+  const handleSearchBarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (userLookup.length === 1) setUserQueryResults([]); // when user deletes entry, search results are removed
+    return setUserLookup(event.currentTarget.value);
+  };
+
+  const handleSearchBarEnterClick = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' || e.code === 'Enter') { // user wants to search
+      return handleUserQueryToDb()
+    } else {
+      return;
+    };
+  };
+
+  const handleUserQueryToDb = async () => {
+    if (userLookup.length < 2) return alert('Your user lookup is too short');
+    const authToken = localStorage.getItem('auth-token');
+    if (typeof authToken === 'undefined') {
+      return alert('You must be signed in and not in incognito to search for users in the database');
+    } else {
+      const apiUrl = `http://127.0.0.1:8000/calendar/userQuery?user=${userLookup}`;
+      const request = await fetch(apiUrl, {
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${authToken}`,
+          'Content-Type': 'application/json'
+        },
+        method: 'GET',
+      });
+      const jsonResponse = await request.json();
+      if (jsonResponse.user_results) {
+        return setUserQueryResults(jsonResponse.user_results);
+      } else {
+        alert('We could not find the user you were looking for')
+        setUserQueryResults([]);
+        setUserLookup('');
+      };
+    };
+  };
 
   return (
     <>
@@ -17,10 +61,28 @@ const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Elemen
         {addUserActivated === false ? 'Add User' : 'Close'}
       </button>
       {addUserActivated === true && 
-        // render search bar with search icon inside, placeholder text for searching
-        // handle search when user clicks search icon or clicks enter
-        // render searched users below search bar with blue plus sign to add
-        <p></p>
+        <div className={styles.addUserToCalendarMainContainer}>
+          <div className={styles.addUserToCalendarSearchContainer}>
+            <input 
+              type="text"
+              aria-label="search"
+              className={styles.addUserToCalendarListInput}
+              placeholder="Search a user..."
+              onChange={(e) => handleSearchBarChange(e)}
+              onKeyDown={(e) => handleSearchBarEnterClick(e)}>
+
+            </input>
+            <img 
+              src={searchSvg}
+              className={styles.searchIconSvg}
+              alt="search icon"
+              onClick={() => handleUserQueryToDb()}>
+            </img>
+          </div>
+          <ul className={styles.addUserToCalendarSearchResultsList}>
+
+          </ul>
+        </div>
       }
     </>
   );
