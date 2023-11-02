9055947cd5a86e474035962b529aee9462aa1640
Andrew Dutson <Dutsandrew@gmail.com>
2023-11-01 21:29:17 -0600

    user list styling

--- src/components/Calendar/AddUserToCalendarList.tsx
+++ src/components/Calendar/AddUserToCalendarList.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useState } from "react";
 import styles from '../../styles/components/Calendar/calendar.module.css';
-import { AddUserToCalendarListProps } from "../../types/interfaces";
+import { AddUserToCalendarListProps, calendarUserQueryResults } from "../../types/interfaces";
 import searchSvg from '../../assets/magnify.svg';
-import { json } from "stream/consumers";
+import plusSvg from '../../assets/plus.svg';
+import uniqid from "uniqid";
 
 const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Element => {
 
@@ -11,8 +12,9 @@ const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Elemen
     addUserActivated 
   } = props;
 
+  const [apiRequestSent, setApiRequestSent] = useState(false);
   const [userLookup, setUserLookup] = useState('');
-  const [userQueryResults, setUserQueryResults] = useState([]);
+  const [userQueryResults, setUserQueryResults] = useState<calendarUserQueryResults>([]);
 
   const handleSearchBarChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (userLookup.length === 1) setUserQueryResults([]); // when user deletes entry, search results are removed
@@ -23,12 +25,12 @@ const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Elemen
     if (e.key === 'Enter' || e.code === 'Enter') { // user wants to search
       return handleUserQueryToDb()
     } else {
-      return;
+      handleSearchBarChange((e as any));
     };
   };
 
   const handleUserQueryToDb = async () => {
-    if (userLookup.length < 2) return alert('Your user lookup is too short');
+    if (userLookup.length === 0) return alert('Your user lookup is too short');
     const authToken = localStorage.getItem('auth-token');
     if (typeof authToken === 'undefined') {
       return alert('You must be signed in and not in incognito to search for users in the database');
@@ -43,6 +45,8 @@ const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Elemen
         method: 'GET',
       });
       const jsonResponse = await request.json();
+      console.log(jsonResponse);
+      setApiRequestSent(true);
       if (jsonResponse.user_results) {
         return setUserQueryResults(jsonResponse.user_results);
       } else {
@@ -51,6 +55,10 @@ const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Elemen
         setUserLookup('');
       };
     };
+  };
+
+  const handleAddUserClickRequest = () => {
+    return;
   };
 
   return (
@@ -69,7 +77,8 @@ const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Elemen
               className={styles.addUserToCalendarListInput}
               placeholder="Search a user..."
               onChange={(e) => handleSearchBarChange(e)}
-              onKeyDown={(e) => handleSearchBarEnterClick(e)}>
+              onKeyDown={(e) => handleSearchBarEnterClick(e)}
+              value={userLookup}>
 
             </input>
             <img 
@@ -79,9 +88,38 @@ const AddUserToCalendarList:FC<AddUserToCalendarListProps> = (props): JSX.Elemen
               onClick={() => handleUserQueryToDb()}>
             </img>
           </div>
-          <ul className={styles.addUserToCalendarSearchResultsList}>
-
-          </ul>
+          {userQueryResults.length > 0 && 
+            <ul className={styles.addUserToCalendarSearchResultsList}>
+              {userQueryResults.map((user) => {
+                return <li 
+                  className={styles.addUserToCalendarListItem}
+                  key={uniqid()}
+                >
+                  <div className={styles.addUserToCalendarListItemTextContainer}>
+                    <p className={styles.addUserToCalendarListText}>
+                      {user.user.first_name ? user.user.first_name : ''}&nbsp;
+                      {user.user.last_name ? user.user.last_name : ''} -&nbsp;
+                      {user.user.job_title ? user.user.job_title : ''},&nbsp;
+                      {user.user.company ? user.user.company : ''}
+                    </p>
+                    <p className={styles.addUserToCalendarEmailText}>
+                      {user.user.email ? user.user.email : ''}
+                    </p>
+                  </div>
+                  <div className={styles.addCalendarUserButtonContainer}>
+                    <img 
+                      src={plusSvg} 
+                      alt="plus icon" 
+                      className={styles.addCalendarUserButton}
+                      onClick={() => handleAddUserClickRequest()}
+                    >
+                    </img>
+                  </div>
+                </li>
+              })}
+            </ul>
+          }
+          {apiRequestSent === true && userQueryResults.length === 0 && <p>No results</p>}
         </div>
       }
     </>
