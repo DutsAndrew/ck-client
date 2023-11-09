adbc9586ef1e3e883167aac65e51a8b569ebaa98
Andrew Dutson <Dutsandrew@gmail.com>
2023-11-08 20:43:29 -0700

    added date snapshots for storing notes in certain slots of a calendar

--- src/components/Calendar/CalendarNavItems/AddNoteForm.tsx
+++ src/components/Calendar/CalendarNavItems/AddNoteForm.tsx
@@ -11,6 +11,94 @@ const AddNoteForm:FC<addNoteFormProps> = (props): JSX.Element => {
     note: '',
   })
 
+  const generateDaySnapshotsForWeek = () => {
+    const today = new Date();
+    const currentDay = today.getDay();
+
+    // calc Monday of week
+    const monday = new Date(today);
+    monday.setDate(today.getDate() - currentDay + 1);
+    const mondayFormatted = monday.toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric' }); // keep it in MM DD format
+
+    // init array for week
+    const datesOfWeek = [mondayFormatted];
+
+    // fill remainder of week
+    for (let i = 1; i < 7; i++) {
+      const nextDay = new Date(monday);
+      nextDay.setDate(monday.getDate() + i);
+      const nextDayFormatted = nextDay.toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric' }); // keep it in MM DD format
+      datesOfWeek.push(nextDayFormatted);
+    };
+
+    return datesOfWeek;
+  };
+
+  const generateWeekSnapshotsForYear = () => {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    const startOfWeek = (date: Date) => {
+      const day = date.getDay();
+      return new Date(date.getFullYear(), date.getMonth(), date.getDate() - (day === 0 ? 6 : day - 1));
+    };
+
+    const endOfWeek = (date: Date) => {
+      const start = startOfWeek(date);
+      const end = new Date(start);
+      end.setDate(start.getDate() + 6);
+      return end;
+    };
+
+    const weekSnapshots: string[] = [];
+
+    let currentDatePointer = new Date(currentYear, 0, 1); // Start from January 1st of the current year
+
+    while (currentDatePointer.getFullYear() === currentYear) {
+      const startDate = startOfWeek(currentDatePointer);
+      const endDate = endOfWeek(currentDatePointer);
+      
+      const startFormatted = startDate.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+      const endFormatted = endDate.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+      const dateFormatted = `${startFormatted} - ${endFormatted}`;
+      
+      weekSnapshots.push(dateFormatted);
+      
+      // Move to the next week
+      currentDatePointer.setDate(currentDatePointer.getDate() + 7);
+    }
+
+    return weekSnapshots;
+  };
+
+  const generateMonthSnapShotsForFiveYears = () => {
+    const calendarMonths = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December",
+    ];
+
+    const fiveYearList: string[] = [];
+    let year = new Date().getFullYear();
+
+    for (let i = 0; i < 6; i++) {
+      calendarMonths.forEach((month) => {
+        const appendYearToMonth = `${month} ${year + i}`
+        fiveYearList.push(appendYearToMonth);
+      });
+    };
+
+    return fiveYearList;
+  };
+
+  const generateYearSnapshotForTenYears = () => {
+    const years: number[] = [];
+    let year = new Date().getFullYear();
+    for (let i = 0; i < 10; i++) {
+      years.push(year + i);
+    };
+    return years;
+  };
+
   const handleFormInputChange = () => {
 
   };
@@ -22,21 +110,16 @@ const AddNoteForm:FC<addNoteFormProps> = (props): JSX.Element => {
   return (
     <div className={styles.addEventFormContainer}>
       <h2 className={styles.addEventHeader}>
-        Event
+        Note
       </h2>
       <form onSubmit={handleFormSubmit} className={styles.addEventForm}>
         <div className={styles.formGroup}>
-          <label 
-            className={styles.addEventFormLabel}
-            htmlFor='note-input'
-            >
-              *Note:
-          </label>
           <textarea
             id='note-input'
             name="note"
             value={formData.note}
             onChange={handleFormInputChange}
+            placeholder="Write a note to self..."
             required
             className={styles.addEventFormInput}
           />
