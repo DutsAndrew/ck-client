0b56b104f308e1f33c484da291891d05f39398cc
DutsAndrew <dutsandrew@gmail.com>
2023-12-14 17:33:14 -0700

    events can be opened to view all the details, svgs are present to perform operations on it

--- src/components/Calendar/EventViewer.tsx
+++ src/components/Calendar/EventViewer.tsx
@@ -1,6 +1,9 @@
 import React, { FC, useEffect } from "react";
 import { eventViewerProps } from "../../types/interfaces";
 import styles from '../../styles/components/Calendar/calendar.module.css';
+import pencilSvg from '../../assets/pencil-outline.svg';
+import trashSvg from '../../assets/delete.svg';
+import { getCalendarEventTimeForLocal } from "../../scripts/calendarHelpers";
 
 const EventViewer:FC<eventViewerProps> = (props): JSX.Element => {
 
@@ -8,6 +11,8 @@ const EventViewer:FC<eventViewerProps> = (props): JSX.Element => {
     event,
     handleCloseEventViewerRequest,
   } = props; // event could be passed as undefined, handle appropriately
+
+  console.log(event)
 
   useEffect(() => {
     temporarilyDisableScrollBar();
@@ -33,6 +38,15 @@ const EventViewer:FC<eventViewerProps> = (props): JSX.Element => {
     body.classList.remove('disableScrollbar');
   };
 
+  // edit button to pull up event form to edit event
+          // trash can icon to delete event
+          // all event details that are not currently displayed:
+            // event date
+            // event description
+            // event handleCalendarTimeChangeRequest
+            // repeats and repeat option
+            // created by
+
   if (typeof event === 'undefined') {
     return (
       <></>
@@ -45,7 +59,42 @@ const EventViewer:FC<eventViewerProps> = (props): JSX.Element => {
         className={styles.eventViewerSectionContainer}
       >
         <div className={styles.eventViewContainer}>
-          {event.event_name}
+          <div className={styles.eventViewSvgContainer}>
+            <img 
+              className={styles.eventViewEditSvg} 
+              alt="edit icon" 
+              src={pencilSvg}>
+            </img>
+            <img 
+              className={styles.eventViewDeleteSvg} 
+              alt="delete icon" 
+              src={trashSvg}>
+            </img>
+          </div>
+          <div className={styles.eventViewDetailContainer}>
+            <p className={styles.eventViewNameText}>
+              <strong>Name: </strong>{event.event_name}
+            </p>
+            <p className={styles.eventViewDescriptionText}>
+              <strong>{event.event_description.length > 0 ? 'Description: ' : ''}</strong>
+              {event.event_description.length > 0 ? <br></br> : ''}
+              {event.event_description.length > 0 ? `${event.event_description}` : ''}
+            </p>
+            <p className={styles.eventViewDateText}>
+              <strong>Date: </strong>{event.event_date.split(" ")[0]}
+            </p>
+            <p className={styles.eventViewTimeText}>
+              <strong>{event.event_time.length > 0 ? 'Time: ' : ''}</strong>
+              {event.event_time.length > 0 ? `${getCalendarEventTimeForLocal(event)}` : ''}
+            </p>
+            <p className={styles.eventViewRepeatText}>
+              <strong>{event.repeats === true ? 'Repeats: ' : ''}</strong>
+              {event.repeats === true ? `${event.repeat_option}` : ''}
+            </p>
+            <p className={styles.eventViewCreatedByText}>
+              <strong>Created by:</strong> {event.created_by.first_name}, {event.created_by.last_name}
+            </p>
+          </div>
         </div>
       </section>
     )
